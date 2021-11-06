ab6f9cda660e2ec05a4880b3f9ac4b66d3b2bed3
Oliver Knox <dev@oliverroyknox.com>
2021-11-06 19:02:38 +0000

    Added custom theme to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@ module.exports = {
     purge: ["./src/**/*.{js,jsx,ts,tsx}"],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "transparent": "transparent",
+                "current": "currentColor",
+                "black": "#232222",
+                "white": "#ffffff",
+                "blue": "#006bca",
+                "dark-raspberry": "#d70040",
+                "pink-raspberry": "#ffdfe3",
+            },
+            fontFamily: {
+                sans: ["Roboto"],
+            },
+            letterSpacing: {
+                tight: "0.02em",
+                normal: "0.035em",
+            },
+        },
     },
     variants: {
         extend: {},
