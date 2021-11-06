14586b6119184ce6d44685b5d87867d80437467c
Oliver Knox <dev@oliverroyknox.com>
2021-11-06 23:40:15 +0000

    Added layout component

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,8 @@
+import * as React from "react";
+import Header from "../Header/Header";
+
+const Layout = ({ children }) => {
+    return [<Header />, <main>{children}</main>];
+};
+
+export default Layout;
