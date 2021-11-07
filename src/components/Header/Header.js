dc07f6372f37d3a386a25e645fcb5e2ad41e9456
Oliver Knox <dev@oliverroyknox.com>
2021-11-07 18:46:41 +0000

    Added key props to components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby";
 
 const NavLabel = ({ text, to }) => {
     return (
-        <Link to={to} className="ml-12 transition-colors hover:text-dark-raspberry">
+        <Link to={to} className="ml-12 aria-current-page transition-colors hover:text-dark-raspberry">
             <span>{text}</span>
         </Link>
     );
@@ -11,7 +11,10 @@ const NavLabel = ({ text, to }) => {
 
 const NavTitle = ({ text, to, isVisible }) => {
     return (
-        <Link to={to} className={`text-4xl font-medium transition-color transition-transform hover:text-dark-raspberry ${isVisible ? "" : "transform translate-x-8"}`}>
+        <Link
+            to={to}
+            className={`text-4xl font-medium aria-current-page transition-color transition-transform hover:text-dark-raspberry ${isVisible ? "" : "transform translate-x-8"}`}
+        >
             <span>{text}</span>
         </Link>
     );
@@ -25,7 +28,7 @@ const HamburgerMenu = () => {
     }, [open]);
 
     return [
-        <div className="md:hidden flex justify-start items-center ml-auto" onClick={() => setOpen(!open)}>
+        <div key="hamburger" className="md:hidden flex justify-start items-center ml-auto" onClick={() => setOpen(!open)}>
             <div className="h-5 w-5 flex flex-col justify-around items-center">
                 <span className={`h-0.5 w-full rounded bg-black transition-transform ${open ? "transform rotate-45 translate-y-[6.25px]" : ""}`}></span>
                 <span className={`${open ? "h-0 w-0" : "h-0.5 w-full"} rounded bg-black transition-all`}></span>
@@ -33,6 +36,7 @@ const HamburgerMenu = () => {
             </div>
         </div>,
         <div
+            key="menu"
             className={`md:hidden smh:gap-16 gap-4 absolute top-16 left-0 bottom-0 right-0 overflow-auto flex flex-col justify-center items-start p-10 bg-white transition-opacity ${
                 open ? "" : "opacity-0 invisible"
             }`}
@@ -49,7 +53,7 @@ const HamburgerMenu = () => {
 const Header = () => {
     return (
         <header className="h-16 w-full flex items-center px-5 text-lg tracking-normal">
-            <Link to="/" className="transition-colors hover:text-dark-raspberry">
+            <Link to="/" className="transition-colors hover:text-dark-raspberry aria-current-page">
                 <span>Oliver Knox</span>
             </Link>
             <div className="hidden md:flex md:justify-start md:items-center md:ml-auto">
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import Header from "../Header/Header";
 
 const Layout = ({ children }) => {
-    return [<Header />, <main>{children}</main>];
+    return [<Header key="header" />, <main key="main">{children}</main>];
 };
 
 export default Layout;
