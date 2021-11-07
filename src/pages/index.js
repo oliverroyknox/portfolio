258ca346134f4ad31700de2c4b9904d396c68b45
Oliver Knox <dev@oliverroyknox.com>
2021-11-07 14:49:46 +0000

    Tweaked responsive styling for text contents

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ const IndexPage = () => {
                 subtitle={<span>The web is essential to the modern world. I love to create rich user experience to make it's power accessible to everyone.</span>}
             />
             <div className="w-full px-10 pb-8 flex justify-center items-center tracking-normal">
-                <div className="max-w-2xl">
+                <div className="max-w-2xl w-full">
                     <h2 className="text-2xl font-medium mb-4 tracking-tight">Who am I?</h2>
                     <ul className="text-xl my-8">
                         <li className="flex justify-start items-center mb-4">
