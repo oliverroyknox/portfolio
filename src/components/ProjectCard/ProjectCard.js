32d5a2d6861caf76bbf36c3bd4841356fc750a67
Oliver Knox <dev@oliverroyknox.com>
2021-11-08 21:36:19 +0000

    Added projects page and project card component

---
+++ src/components/ProjectCard/ProjectCard.js
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { Link } from "gatsby";
+import books from "../../images/vinyl.jpeg";
+
+const ProjectCard = ({ title, src, alt, href }) => {
+    return (
+        <Link to={href}>
+            <div className="w-full h-96 rounded-xl overflow-hidden relative group transition-all duration-300 pointer:hover:scale-105">
+                <img
+                    src={src}
+                    alt={alt}
+                    className="h-full w-full object-cover filter pointer:blur-sm transition-all duration-300 pointer:group-hover:blur-none opacity-80 pointer:opacity-100"
+                />
+                <div className="rounded-xl font-medium text-white bg-black w-2/3 border-l-[0.75rem] border-pink-raspberry absolute bottom-5 left-5 py-2 px-4 text-2xl transition-all duration-300 pointer:group-hover:translate-y-24 pointer:group-hover:-translate-x-12 pointer:group-hover:scale-50">
+                    {title}
+                </div>
+            </div>
+        </Link>
+    );
+};
+
+export default ProjectCard;
---
+++ src/pages/projects.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+import Layout from "../components/Layout/Layout";
+import Hero from "../components/Hero/Hero";
+import ProjectCard from "../components/ProjectCard/ProjectCard";
+import blog from "../images/blog.png";
+
+const ProjectsPage = () => {
+    return (
+        <Layout>
+            <Hero
+                title={
+                    <span>
+                        Here is some of the <span className="text-dark-raspberry">projects</span> I’ve worked on.
+                    </span>
+                }
+                subtitle={
+                    <span>I’m always thinking about new ideas, and hopefully some of them are useful. All the source code for these projects can be found on my GitHub page.</span>
+                }
+            />
+            <div className="w-full px-10 pb-8 flex justify-center items-center tracking-normal">
+                <div className="md:px-10 w-full grid md:grid-cols-2 lg:grid-cols-3 auto-rows-auto gap-8">
+                    <ProjectCard title="Blog" href="https://blog.oliverroyknox.com" src={blog} alt="My personal blog" />
+                </div>
+            </div>
+        </Layout>
+    );
+};
+
+export default ProjectsPage;
